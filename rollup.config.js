0c417c02e03f8f78dcdcef3fb195d27c30f7502b
liuying <wocanbe@163.com>
2021-09-04 00:24:17 +0800

    初步完成ts的编译命令

--- rollup.config.js
+++ rollup.config.js
@@ -1,46 +1,35 @@
-import babel from 'rollup-plugin-babel'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import typescript from '@rollup/plugin-typescript'
 import {terser} from 'rollup-plugin-terser'
 import copy from 'rollup-plugin-copy'
 
-const globals = {
-  'babel-runtime/core-js/json/stringify': 'BabelCore.JsonStringfy',
-  'babel-runtime/core-js/promise': 'BabelCore.Promise',
-  'babel-runtime/core-js/symbol': 'BabelCore.symbol',
-  'babel-runtime/regenerator': 'BabelCore.regenerator',
-  'babel-runtime/helpers/asyncToGenerator': 'BabelHelpers.asyncToGenerator',
-  'babel-runtime/helpers/classCallCheck': 'BabelHelpers.classCallCheck',
-  'babel-runtime/helpers/createClass': 'BabelHelpers.createClass'
-}
-export default {
-  input: 'src/Ajax.js',
-  output: {
-    file: 'dist/index.umd.js',
-    format: 'umd',
-    name: 'luck7-fetch',
-    globals,
-    banner: '/**\n* Copyright 584069777@qq.com 2018.\n*/'
-  },
-  // plugins: [babel({runtimeHelpers: true})],
-  plugins: [
-    babel({
-      babelrc: false,
-      runtimeHelpers: true,
-      'presets': [
-        [require.resolve('babel-preset-env'), {modules: false}]
-      ],
-      plugins: [require.resolve('babel-plugin-transform-runtime')],
-      'comments': true
-    }),
-    terser(),
-    copy({
-      targets: [
-        { src: 'src/Ajax.d.ts', dest: 'dist', rename: 'index.d.ts' }
-      ]
-    })
-],
-  external: (id) => {
-    if (/^babel-runtime\/.*$/.test(id)) {
-      return true
-    }
+function getConfig (format) {
+  const config = {
+    input: 'src/Ajax.ts',
+    output: {
+      file: 'dist/index.' + format + '.js',
+      format: format,
+      name: 'luck7-fetch',
+      sourceMap: true
+    },
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript(),
+      terser()
+    ]
   }
+  if (format === 'esm') {
+    config.plugins.push(copy({
+      targets: [
+        { src: 'types/global.d.ts', dest: 'dist', rename: 'index.d.ts' }
+      ]
+    }))
+  }
+  return config
 }
+export default [
+  getConfig('cjs'), 
+  getConfig('esm')
+]
--- src/Ajax.ts
+++ src/Ajax.ts
@@ -5,18 +5,19 @@ import {checkList, checkItem, checkAllowMethod, getAjaxConfig} from './lib/tools
 const dafaultConfig = {
   timeout: 120000 // 默认超时2分钟
 }
+let fetch:(url: string, options?: any) => Promise<any> = window.fetch
 class Ajax {
-  private strict:boolean = true;
-  private baseURL:string = '/';
-  private apis:apiList = {};
-  private methods:apiMethod = {};
-  private defaultCfg:any = {};
-  private fetch:(url: string, options?: any) => Promise<any> = window.fetch
+  #strict:boolean = true;
+  #baseURL:string = '/';
+  #apis:apiList = {};
+  #methods:apiMethod = {};
+  #defaultCfg:any = {};
   constructor (apiList:apiList, configs?:ajaxConfig, apiMethods?:apiMethod) {
-    this.init(apiList, configs, apiMethods)
-    if (arguments[0].init) this.do('init')
+    this.#init(apiList, configs, apiMethods)
+    if (apiList.init) this.do('init')
   }
-  init (apiList:apiList, configs?:ajaxConfig, apiMethods?:apiMethod) {
+
+  #init (apiList:apiList, configs?:ajaxConfig, apiMethods?:apiMethod) {
     const options = mergeJson(dafaultConfig, configs.options)
     if (options.method) {
       options.method = options.method.toUpperCase()
@@ -27,55 +28,59 @@ class Ajax {
         if (defaultLang[key]) defaultLang[key] = configs.lang[key]
       }
     }
-    if (isBool(configs.isStrict)) this.strict = configs.isStrict
-    if (configs.baseURL) this.baseURL = configs.baseURL
-    if (apiList) this.apis = checkList(apiList)
+    if (isBool(configs.isStrict)) this.#strict = configs.isStrict
+    if (configs.baseURL) this.#baseURL = configs.baseURL
+    if (apiList) this.#apis = checkList(apiList)
     else throw new Error(defaultLang.noList)
 
-    this.methods = apiMethods
-    this.defaultCfg = options
+    this.#methods = apiMethods
+    this.#defaultCfg = options
   }
+
   do (apiName:Array<string|api>|string|api, params?:any):Promise<any> {
     if (isArr(apiName)) {
       const ajaxReqs = []
       const apis = apiName as Array<string|api>
       for (let order:number = 0; order < apis.length; order++) {
-        ajaxReqs.push(this.single(apis[order], params[order]))
+        ajaxReqs.push(this.#single(apis[order], params[order]))
       }
       return Promise.all(ajaxReqs)
-    } else return this.single((apiName as string|api), params)
+    } else return this.#single((apiName as string|api), params)
   }
+
   setFetch (fetchFun:(url: string, options?: any) => Promise<any>) {
-    this.fetch = fetchFun
+    fetch = fetchFun
   }
-  getCfg (apiName:string|api, params?:any) {
+
+  #getCfg (apiName:string|api, params?:any) {
     let localConfig
     if (isStr(apiName)) {
-      const apiConfig = this.apis[apiName as string]
+      const apiConfig = this.#apis[apiName as string]
       if (apiConfig) localConfig = cloneJson(apiConfig)
-      else if (this.strict) throw new Error(defaultLang.noConfig.replace('#apiName#', apiName as string))
+      else if (this.#strict) throw new Error(defaultLang.noConfig.replace('#apiName#', apiName as string))
       else localConfig = {url: apiName}
     } else {
-      if (this.strict) throw new Error(defaultLang.typeError)
+      if (this.#strict) throw new Error(defaultLang.typeError)
       if (isObj(apiName)) localConfig = checkItem(apiName, '')
       else throw new Error(defaultLang.typeError)
     }
-    if (localConfig.options) localConfig.options = mergeJson(this.defaultCfg, localConfig.options)
-    else localConfig.options = this.defaultCfg
+    if (localConfig.options) localConfig.options = mergeJson(this.#defaultCfg, localConfig.options)
+    else localConfig.options = this.#defaultCfg
     const reqConfig = getAjaxConfig(localConfig, params)
     return reqConfig
   }
-  single (apiName:string|api, params?:any) {
+
+  #single (apiName:string|api, params?:any) {
     let reqUrl:string, reqConfig:any
     try {
-      const lCfg = this.getCfg(apiName, params)
+      const lCfg = this.#getCfg(apiName, params)
       reqUrl = lCfg[0]
       reqConfig = lCfg[1]
     } catch (e) {
       return Promise.reject(e)
     }
-    let filterFun = this.methods['_']
-    if (isStr(apiName)) filterFun = this.methods[apiName as string]
+    let filterFun = this.#methods['_']
+    if (isStr(apiName)) filterFun = this.#methods[apiName as string]
     let sucessFun:Function, errorFun:Function
     if (isFun(filterFun)) {
       const filterObj = filterFun()
@@ -94,7 +99,7 @@ class Ajax {
         if (filterObj.response) sucessFun = filterObj.response
       }
     }
-    reqUrl = this.baseURL + reqUrl
+    reqUrl = this.#baseURL + reqUrl
     let reqData = reqConfig.data
     if (reqConfig.method === 'GET') {
       reqUrl = reqUrl + json2query(reqData)
@@ -109,7 +114,7 @@ class Ajax {
       const outNo = setTimeout(() => reject(new Error('fetch timeout')), reqConfig.timeout)
       delete reqConfig.timeout
       try {
-        const res = await this.fetch(reqUrl, reqConfig)
+        const res = await fetch(reqUrl, reqConfig)
         if (res.status == 200) {
           const resData = await res.json()
           const data = sucessFun ? sucessFun(resData) : resData
